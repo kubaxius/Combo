e96c5317ef00d4ef54ec4b79806d7b11fdf1759b
Jakub Niedźwiedź <jakub.niedzwiedz98@gmail.com>
2024-11-30 14:44:54 +0100

    Split code into fuctions

--- bin/components/leave_trail.gd
+++ bin/components/leave_trail.gd
@@ -5,33 +5,46 @@ class_name LeaveTrailComp extends Node2D
 
 @onready var trail_viewport: TrailMaskGen = get_tree().get_first_node_in_group("trail_texture_generator_viewport")
 @onready var full_trail_viewport:SubViewport = trail_viewport.get_node("FullTrailMaskGen")
+
+## Only enable if required nodes exist.
 @onready var active = trail_viewport and full_trail_viewport
+
 @onready var parent:Node2D = get_parent()
+
 @onready var sprite := Sprite2D.new()
 @onready var parallax := Parallax2D.new()
 
-var mask_scaling
 var red_shader:VisualShader = preload("res://shaders/color_to_red.tres")
 
 func _ready() -> void:
 	if active:
-		mask_scaling = trail_viewport.mask_scaling
-		sprite.texture = texture
-		sprite.scale = Vector2(1./mask_scaling.x, 1./mask_scaling.y)
-		sprite.material = ShaderMaterial.new()
-		sprite.z_index = 2
-		var mat:ShaderMaterial = sprite.material
-		mat.shader = red_shader
-		full_trail_viewport.add_child(parallax)
-		parallax.add_child(sprite)
-		parallax.follow_viewport = false
-		parallax.ignore_camera_scroll = true
-		parallax.repeat_size = full_trail_viewport.size
-		parallax.repeat_times = 4
-		
+		_setup_sprite()
+		_setup_parallax()
+	
+	set_physics_process(active)
 
 
 func _physics_process(_delta: float) -> void:
-	if active:
-		parallax.screen_offset = -parent.global_position/mask_scaling
-		sprite.global_rotation = parent.global_rotation
+	_copy_parents_transform()
+
+
+func _setup_sprite():
+	sprite.texture = texture
+	sprite.scale = Vector2(1./trail_viewport.mask_scaling.x, 1./trail_viewport.mask_scaling.y)
+	sprite.material = ShaderMaterial.new()
+	sprite.z_index = 2
+	sprite.material.shader = red_shader
+
+
+func _setup_parallax():
+	parallax.add_child(sprite)
+	full_trail_viewport.add_child(parallax)
+	parallax.follow_viewport = false
+	parallax.ignore_camera_scroll = true
+	parallax.repeat_size = full_trail_viewport.size
+	parallax.repeat_times = 4
+
+
+func _copy_parents_transform():
+	parallax.screen_offset = -parent.global_position/trail_viewport.mask_scaling
+	sprite.global_rotation = parent.global_rotation
