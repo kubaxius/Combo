f1c69b8adfcaf175c4f2cf727f16b8efa9b492e7
Jakub Niedźwiedź <jakub.niedzwiedz98@gmail.com>
2024-12-01 12:56:24 +0100

    Add ground detection to worm head

--- bin/worm/base/head/head.gd
+++ bin/worm/base/head/head.gd
@@ -8,9 +8,20 @@ var turning_speed
 var desired_movement_vector = Vector2.UP
 
 
-func _physics_process(delta: float) -> void:
+func _grounded_state_physics_processing(delta: float) -> void:
 	if speed == 0:
 		return
 	
 	rotation = lerp_angle(rotation, desired_movement_vector.angle() + PI/2, delta*turning_speed)
 	move_and_collide(Vector2.UP.rotated(rotation) * speed * delta)
+
+
+func _airborn_state_physics_processing(delta: float) -> void:
+	rotation = lerp_angle(rotation, Vector2.DOWN.angle() + PI/2, delta*turning_speed)
+	move_and_collide(Vector2.UP.rotated(rotation) * speed * delta)
+
+func _on_ground_checker_comp_grounded_state_changed(grounded: bool, last_ground: Node2D) -> void:
+	if grounded:
+		$PhysicsStateChart.send_event("segment_entered_ground")
+	else:
+		$PhysicsStateChart.send_event("segment_exited_ground")
