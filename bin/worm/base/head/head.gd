2fb4be1b045b8a9e03b02cffaa5aead3ec45f0f9
Jakub Niedźwiedź <jakub.niedzwiedz98@gmail.com>
2024-12-01 19:35:26 +0100

    Change worm physics
    Now the head moves first, and only then rotates where it moves. Also there is gravity and air drag.

--- bin/worm/base/head/head.gd
+++ bin/worm/base/head/head.gd
@@ -5,23 +5,53 @@ var speed
 var turning_speed
 
 ## set by components
-var desired_movement_vector = Vector2.UP
+#TODO: make it set by signals instead
+var desired_movement_vector := Vector2.UP
+
+## in m/s
+var gravity = 19.8
+var air_resistance = 0.02
+
+
+func _ready() -> void:
+	velocity = Vector2.UP
+
+
+func _on_grounded_state_entered() -> void:
+	pass
 
 
 func _grounded_state_physics_processing(delta: float) -> void:
-	if speed == 0:
-		return
-	
-	rotation = lerp_angle(rotation, desired_movement_vector.angle() + PI/2, delta*turning_speed)
-	move_and_collide(Vector2.UP.rotated(rotation) * speed * delta)
+	velocity = lerp(velocity.length(), speed, 2.0 * delta) * velocity.normalized()
+	var target_angle = lerp_angle(velocity.angle(), desired_movement_vector.angle(), 2.0 * delta)
+	velocity = velocity.rotated(target_angle - velocity.angle())
 
 
 func _airborn_state_physics_processing(delta: float) -> void:
-	rotation = lerp_angle(rotation, Vector2.DOWN.angle() + PI/2, delta*turning_speed)
-	move_and_collide(Vector2.UP.rotated(rotation) * speed * delta)
+	_apply_gravity(delta)
+	_apply_air_resistance(delta)
 
-func _on_ground_checker_comp_grounded_state_changed(grounded: bool, last_ground: Node2D) -> void:
+
+func _physics_process(delta: float) -> void:
+	print(Utils.pps_to_kmph(velocity.length()))
+	move_and_slide()
+	_look_forward()
+
+
+func _on_ground_checker_comp_grounded_state_changed(grounded: bool, _last_ground: Node2D) -> void:
 	if grounded:
 		$PhysicsStateChart.send_event("segment_entered_ground")
 	else:
 		$PhysicsStateChart.send_event("segment_exited_ground")
+
+
+func _look_forward() -> void:
+	rotation = velocity.angle() + PI/2.
+
+
+func _apply_gravity(delta: float) -> void:
+	velocity.y += Utils.mps_to_pps(gravity) * delta
+
+
+func _apply_air_resistance(delta: float) -> void:
+	velocity *= pow(1. - air_resistance, delta)
